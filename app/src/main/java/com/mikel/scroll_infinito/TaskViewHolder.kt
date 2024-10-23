3660ec53ee101a43b5a55a72d31cd307249fe2c3
mikel <mikel.ra.2003@gmail.com>
2024-10-23 13:28:42 +0200

    add:Diseño de celdas y ViewHolder

---
+++ app/src/main/java/com/mikel/scroll_infinito/TaskViewHolder.kt
@@ -0,0 +1,16 @@
+package com.mikel.scroll_infinito
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    private val tvTask:TextView = view.findViewById(R.id.tvTask)
+
+    fun render (task:String){
+        tvTask.text=task
+    }
+
+
+}
