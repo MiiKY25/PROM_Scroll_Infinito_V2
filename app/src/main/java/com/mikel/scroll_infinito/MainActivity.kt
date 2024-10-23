9e5cd843d7968dd4de5290864a998faedd01d50b
mikel <mikel.ra.2003@gmail.com>
2024-10-23 13:33:46 +0200

    add:Main Activity

--- app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.mikel.scroll_infinito
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAddTask:Button
+    lateinit var etTask:EditText
+    lateinit var rvTasks:RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initUi()
 
+    }
 
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+    private fun initUi(){
+        initView()
+        initListeners()
+    }
+
+    private fun initListeners(){
+
+    }
+
+    private fun initView(){
+
     }
 }
