2ead66deaf743ec9545d312fb4d920df5dbf2d54
mikel <mikel.ra.2003@gmail.com>
2024-10-23 14:08:14 +0200

    add:mostrando tareas

--- app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnAddTask:Button
     lateinit var etTask:EditText
     lateinit var rvTasks:RecyclerView
+
+    lateinit var adapter: TaskAdapter
 
     var tasks = mutableListOf<String>()
 
@@ -27,13 +30,29 @@ class MainActivity : AppCompatActivity() {
     private fun initUi(){
         initView()
         initListeners()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        adapter = TaskAdapter(tasks)
+        rvTasks.adapter=adapter
     }
 
     private fun initListeners(){
-        //btnAddTask.setOnClickListener{ addTask() }
+        btnAddTask.setOnClickListener{ addTask() }
+    }
+
+    private fun addTask(){
+        val taskToAdd:String = etTask.text.toString()
+        tasks.add(taskToAdd)
+        adapter.notifyDataSetChanged()
+        etTask.setText("")
     }
 
     private fun initView(){
-
+        btnAddTask = findViewById(R.id.btn_aniadirTarea)
+        etTask = findViewById(R.id.txt_tarea)
+        rvTasks = findViewById(R.id.View_listaTarea)
     }
 }
