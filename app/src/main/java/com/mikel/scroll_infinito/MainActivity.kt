185affdd40c87fdeebaf573291deb27353b6786f
MiiKY <mikel.ra.2003@gmail.com>
2024-10-25 17:11:08 +0200

    add:crear BBDD

--- app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/mikel/scroll_infinito/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.mikel.scroll_infinito
 
-import android.icu.text.Transliterator.Position
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
 
